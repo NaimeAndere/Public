f840d41804235b8a5af14a03bfde1966c7faa500
Naime Andere <naimera@gmail.com>
2024-05-28 23:12:10 -0300

    Added Idle.ps1

---
+++ Powershell Scripts/Idle.ps1
@@ -0,0 +1,20 @@
+﻿# ------------------------------------------------------------
+# Description: Script creator to avoid computer lock automatically. each tine defined, Numlock buton will be on/off and after 1milisec will off/on again.
+# Criador: Naime Andere https://www.linkedin.com/in/naimeandere/
+# Data: Fev/2022
+# Versão: 1.0
+# ------------------------------------------------------------
+
+ Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+ cls
+ Write-Host "Continuos Process Running..."
+# Loop infinito
+while ($true) {
+    $wsh = New-Object -ComObject WScript.Shell
+    $wsh.SendKeys('{NUMLOCK}')
+Start-Sleep -Milliseconds 1
+    $wsh = New-Object -ComObject WScript.Shell
+    $wsh.SendKeys('{NUMLOCK}')
+Start-Sleep -Seconds 120
+
+}
